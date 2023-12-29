2cf7d7888eba6559ce3115cb6f560d19370cfe52
chahat30 <chahatba9876@gmail.com>
2023-12-29 20:38:15 +0530

    register frontend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes,Route} from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage';
+import RegisterPage from './pages/register/RegisterPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index path="/" element={<Home/>}/>
         <Route path="/blog/:id" element={<ArticleDetailPage/>}/>
+        <Route index path="/register" element={<RegisterPage/>}/>
       </Routes>
       </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
-
+import {Provider} from "react-redux";
+import store from './store';
+import {QueryClientProvider,QueryClient} from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+  <QueryClientProvider>
+  <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>    
   </BrowserRouter>
 );
